973d7a42c7d7c633e6ea87883df500a73c219cd7
Rimo <RimoChan@stu.xmu.edu.cn>
2019-04-21 15:04:56 +0800

    切詞去重

--- 詞源.py
+++ 詞源.py
@@ -19,8 +19,11 @@ class 詞源(詞空間.詞空間):
         
     def 切(self, 單詞):
         self.切詞表.append(單詞)
+        self.存檔()
+        
+    def 存檔(self):
         with open('./程序資料/切詞.json', 'w') as f:
-            json.dump(self.切詞表, f)
+            json.dump(list(set(self.切詞表)), f)
         
     def 產生混淆組(self):
         l = list(self.單詞表)
